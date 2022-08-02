d9ce7e1b12ebb661a380bb20ea08a4eaaa1e8974
budaejjigae <iamcheokl@hotmail.com>
2022-08-02 14:33:13 +1000

    columbus

---
+++ columbus/columbus.R
@@ -0,0 +1,14 @@
+library(rgdal)
+library(spdep)
+
+setwd("~/Downloads/BRU0031/columbus")
+
+columbus = readOGR(dsn = ".", layer = "columbus")
+columbus_queen = read.gal(file = "columbus.gal", region.id = columbus$POLYID)
+plot(columbus)
+plot(columbus_queen, coordinates(columbus), add = TRUE)
+
+
+W = nb2listw(neighbours = columbus_queen)
+W = listw2mat(W)
+View(W)
